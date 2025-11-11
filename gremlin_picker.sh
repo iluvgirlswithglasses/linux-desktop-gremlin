419a2c32ca6da6df3792bdc3c75ad2525dd76fdf
Xgameisdabest <lemonadepham@gmail.com>
2025-11-11 12:00:56 +0700

    updated gremlin_picker menu script with back and exit option

--- gremlin_picker.sh
+++ gremlin_picker.sh
@@ -5,9 +5,9 @@
 # get the directory where this script is *actually* located (even if symlinked)
 SOURCE="${BASH_SOURCE[0]}"
 while [ -L "$SOURCE" ]; do
-    DIR="$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)"
-    SOURCE="$(readlink "$SOURCE")"
-    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+	DIR="$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)"
+	SOURCE="$(readlink "$SOURCE")"
+	[[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
 done
 SCRIPT_DIR="$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)"
 
@@ -15,27 +15,36 @@ SCRIPT_DIR="$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)"
 available_gremlins=$(command ls -1 "$SCRIPT_DIR/spritesheet" 2>/dev/null)
 
 # use rofi to pick the selected gremlin
-pick=$(echo -e "$available_gremlins" | rofi -dmenu)
 
-if [[ -z $pick ]]; then
-	exit 0
-fi
+main_menu() {
+	pick=$(echo -e "$available_gremlins\nExit" | rofi -dmenu)
 
-# check which session to launch (use uv)
-if [[ $XDG_SESSION_TYPE == "wayland" ]]; then
-	#check whether if they want to use uv or not
-	pick_uv=$(echo -e "Uv\nNon-uv" | rofi -dmenu)
-	if [[ $pick_uv == "Uv" ]]; then
-		"$SCRIPT_DIR/run-uv-xwayland.sh" "$pick"
-	elif [[ $pick_uv == "Non-Uv" ]]; then
-		"$SCRIPT_DIR/run-xwayland.sh" "$pick"
+	if [[ -z $pick || $pick == "Exit" ]]; then
+		exit 0
 	fi
-elif [[ $XDG_SESSION_TYPE == "x11" ]]; then
-	#check whether if they want to use uv or not
-	pick_uv=$(echo -e "Uv\nNon-uv" | rofi -dmenu)
-	if [[ $pick_uv == "Uv" ]]; then
-		"$SCRIPT_DIR/run-uv-x11.sh" "$pick"
-	elif [[ $pick_uv == "Non-Uv" ]]; then
-		"$SCRIPT_DIR/run-x11.sh" "$pick"
+
+	# check which session to launch (use uv)
+	if [[ $XDG_SESSION_TYPE == "wayland" ]]; then
+		#check whether if they want to use uv or not
+		pick_uv=$(echo -e "Uv\nNon-uv\nBack" | rofi -dmenu)
+		if [[ $pick_uv == "Uv" ]]; then
+			"$SCRIPT_DIR/run-uv-xwayland.sh" "$pick"
+		elif [[ $pick_uv == "Non-uv" ]]; then
+			"$SCRIPT_DIR/run-xwayland.sh" "$pick"
+		elif [[ $pick_uv == "Back" ]]; then
+			main_menu
+		fi
+	elif [[ $XDG_SESSION_TYPE == "x11" ]]; then
+		#check whether if they want to use uv or not
+		pick_uv=$(echo -e "Uv\nNon-uv\nBack" | rofi -dmenu)
+		if [[ $pick_uv == "Uv" ]]; then
+			"$SCRIPT_DIR/run-uv-x11.sh" "$pick"
+		elif [[ $pick_uv == "Non-uv" ]]; then
+			"$SCRIPT_DIR/run-x11.sh" "$pick"
+		elif [[ $pick_uv == "Back" ]]; then
+			main_menu
+		fi
 	fi
-fi
+}
+
+main_menu
